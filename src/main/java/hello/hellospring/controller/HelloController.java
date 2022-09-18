00ffa4c4b0f03121d29f7fd226a0b2b50ea771c6
heyjeong-go <74603471+heyjeong-go@users.noreply.github.com>
2022-09-18 17:13:50 +0900

    스프링부트 강의 - 정적 콘텐츠, MVC 정리

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,13 +3,26 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model) {
+
         model.addAttribute("data", "hello!!");
         return "hello"; // hello.html 찾아줌
+
+    }
+
+    @GetMapping("hello-mvc")
+    // public String helloMvc(@RequestParam(value = "name", required = false) String
+    // name, Model model) {
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+
+        model.addAttribute("name", name);
+        return "hello-template";
+
     }
 }
